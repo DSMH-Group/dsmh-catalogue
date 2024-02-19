bd8be996de20075015737bcee98e9a9aad087aae
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-02-20 03:15:10 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var currentGrid = document.getElementById('current');
+var msnryCurrent = new Masonry( currentGrid, {
+	// options for grid1
+	itemSelector: '.grid-item',
+	columnWidth: 150
+});
+
+var completedGrid = document.getElementById('completed');
+var msnry2 = new Masonry( completedGrid, {
+	// options for grid2
+	itemSelector: '.grid-item',
+	columnWidth: 150
+});
+
+var droppedGrid = document.getElementById('dropped');
+var msnry2 = new Masonry( droppedGrid, {
+	// options for grid2
+	itemSelector: '.grid-item',
+	columnWidth: 150
+});
